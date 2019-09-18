ce266244edb5ad39c4b4c5e11ed129f687491e71
James Martin <james.martin2@bglgroup.co.uk>
2019-09-18 11:36:52 +0100

    Added a readme and created variables for starting X and Y

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace knightJourney
     {
         static void Main(string[] args)
         {
+            int startingX = 0;
+            int startingY = 0;
 
             Board myBoard = new Board(); //Define a board
             myBoard.InitiateBoard();
 
-            Square firstSquare = myBoard.GetSquare(4, 4); // Define a starting Square
+            Square firstSquare = myBoard.GetSquare(startingX, startingY); // Define a starting Square
             firstSquare.moveToSquare();
             Square nextSquare = myBoard.getNextMove(firstSquare.getX(), firstSquare.getY()); // Get next square
             firstSquare.moveFromSquare(); // Move from old square
